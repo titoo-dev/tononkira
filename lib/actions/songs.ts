6e005e854a7fc0cf49121e7e3193217be24e55df
Ony <onyrakoto27@gmail.com>
2025-04-11 17:20:54 +0300

    Rename isSync field to isSynced and implement createSong function in songs.ts

--- lib/actions/songs.ts
+++ lib/actions/songs.ts
@@ -1,3 +1,18 @@
 "use server";
 
-export async function createSong() {}
+import { prisma } from "@/lib/prisma";
+import { Prisma } from "@/lib/generated/prisma";
+
+export async function createSong() {
+  await prisma.song.create({
+    data: {},
+  });
+}
+
+export type CreateSongInput = {
+  title: string;
+  lyrics: {
+    content: string;
+    isSynced: boolean;
+  };
+};
