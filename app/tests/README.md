82d367adebf4ec4bb870b50ad2bc19aab7ca17e5
Titosy Manankasina <dev.titosy@gmail.com>
2025-04-16 16:43:18 +0300

    Remove obsolete test files for server actions and song creation

--- app/tests/README.md
+++
@@ -1,2 +0,0 @@
-This folder is for testing server actions.
-This will be accessible in production.
--- app/tests/routes.ts
+++
@@ -1,5 +0,0 @@
-export function GET() {
-  return new Response(
-    `This folder is for testing server actions.\nThis will be accessible in production.`,
-  );
-}
